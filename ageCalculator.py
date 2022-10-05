e646cbf819b29c4dd086d096e0362b5b530e70a8
JelyAlmutaali <jeyk1ll3r05@gmail.com>
2022-10-05 22:42:13 +0700

    Age Calculator

---
+++ ageCalculator.py
@@ -0,0 +1,7 @@
+# this created by Moh. Jeli Almuta'ali || 20220040244
+
+# get birth year 
+birth_year = int(input("input your birth year! : "))
+this_year = int(input("input this year! : "))
+age = this_year - birth_year
+print("Age: ",age)
