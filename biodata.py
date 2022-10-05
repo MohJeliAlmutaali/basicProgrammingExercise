2529ea2e563a7174f4be5be9003da9af95fef89c
JelyAlmutaali <jeyk1ll3r05@gmail.com>
2022-10-05 22:17:10 +0700

    Biodata

---
+++ biodata.py
@@ -0,0 +1,6 @@
+name = "Moh. Jeli Almuta'ali"
+Class = "TI22I"
+major = "Informatics Engineering"
+quote = "don't sleep before you get something new"
+
+print(f"Name : {name}\nClass: {Class}\nMajor: {major}\nQuote: {quote}")
