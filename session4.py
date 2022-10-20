a100229645ad9e814da8eaf5bdea3b8cb0041bb8
MohJeliAlmutaali <114728937+MohJeliAlmutaali@users.noreply.github.com>
2022-10-21 06:48:25 +0700

    Update session4.py

--- session4.py
+++ session4.py
@@ -40,15 +40,15 @@ def main():
         product_name = input("Produk name: ").title()
         prd_name.append(product_name)
         # harga produk
-        product_price = float(input("Prices: "))
-        prd_price.append(product_price)
+        purchase_price = float(input("Prices: "))
+        prd_price.append(purchase_price)
         # banyak barang yang diorder
         order_quantity = int(input("Order Quantity: "))
         ordr_quantity.append(order_quantity)
 
         # harga beli = harga produk * jumlah order
-        buying_price = product_price * order_quantity
-        buy_price.append(buying_price)
+        total_cost = purchase_price * order_quantity
+        buy_price.append(total_cost)
 
         # keuntungan
         percent = input("Percent: ").split("%")
@@ -57,11 +57,11 @@ def main():
         prcnt.append(percent)
         
         
-        profit = (product_price * float(percent[0])/100 )
+        profit = (purchase_price * float(percent[0])/100 )
         prft.append(profit)
 
         # harga jual = harga produk + profit
-        selling_price = product_price + profit
+        selling_price = purchase_price + profit
         sllng_price.append(selling_price)
         # barang yang terjual
         item_sold = int(input("Item sold: "))
@@ -70,7 +70,7 @@ def main():
         income = item_sold * selling_price
         ttl_income.append(income)
         # total keuntungan
-        total_profit = income - buying_price
+        total_profit = income - total_cost
         ttl_prft.append(total_profit)
         # Menginput lagi
         enter_again = input("\nEnter Again, y/n: ").lower()
@@ -87,15 +87,15 @@ def main():
     sum_list(ttl_prft, t_profit)
 
     print("="*45)
-    print(f"Product Name   : {prd_name}\nPrice          : {prd_price}\nOrder Quantity : {ordr_quantity}\n\
-Buying price   : {buy_price} \nPercent        : ",end='')
+    print(f"Product Name   : {prd_name}\nPurchase Price : {prd_price}\nOrder Quantity : {ordr_quantity}\n\
+Total Cost     : {buy_price} \nPercent        : ",end='')
     if len(prcnt)> 1: 
         for i in range(0, len(prcnt[0])):
             print(prcnt[i][0],"%", end=", ")
     else:
         print(prcnt[0][0],"%")
     print(f"\nMargin Profit  : {prft}\nSelling Price  : {sllng_price}\n\
-Item sold      : {itm_sld}\nIncome         : {ttl_income}\nTotal Income   : {t_income[0]}\nProfit         : {ttl_prft}\nTotal Profit   : {t_profit[0]}")
+Item sold      : {itm_sld}\nIncome         : {ttl_income}\nTotal Income   :  {t_income[0]}\nProfit         : {ttl_prft}\nTotal Profit   :  {t_profit[0]}")
     
     tanda = 45
     if len(prd_name) >= 4:
